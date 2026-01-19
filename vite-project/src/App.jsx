8255ad705dc78b638e3f83978420e26d2f4b4d72
Akshat Sharma <your-personal-email@gmail.com>
2026-01-19 19:36:24 +0530

    Fix SCSS imports and remove duplicate variables
    - Add proper imports from _colors.scss to Home.scss
    - Add proper imports from _colors.scss to Login.scss
    - Add proper imports from _colors.scss to Password.scss
    - Remove duplicate variable definitions
    - Ensure all SCSS files use centralized color variables

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,16 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login/Login'
 import Password from './Pages/Password/Password'
 import Signup from './Pages/Signup/Signup'
-
+import Home from './Pages/Home/Home'
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} />
         <Route path="/password" element={<Password />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<Login />} />
+        <Route path="*" element={<Login />} /> */}
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ vite-project/src/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../../assets/logo_icon.png'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className="Home">
+        <section className="Topbar">
+            <div className="logo">
+                <img src={Logo} alt="Logo" />
+            </div>
+        </section>
+
+      
+    </div>
+  )
+}
+
+export default Home
