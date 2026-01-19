3130daf67c9dbf98778854834c456a176459bdc4
Akshat Sharma <your-personal-email@gmail.com>
2026-01-19 11:26:46 +0530

    Fix imports after file reorganization
    - Update App.jsx imports to reflect new file structure
    - Add Signup import from ./Pages/Signup/Signup
    - Add /signup route to routing
    - Remove old Login/Signup folder references

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login/Login'
 import Password from './Pages/Password/Password'
+import Signup from './Pages/Signup/Signup'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/password" element={<Password />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
